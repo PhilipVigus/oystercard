67e85e09ddaebeea09225a166f32fed768fe171e
Phil Vigus <philipvigus@yahoo.co.uk>
2020-03-23 16:07:54 +0000

    Topped up card updates balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,7 +2,14 @@ require 'oystercard'
 
 describe Oystercard do
   it 'should have a balance of 0 when it is created' do
-    card = Oystercard.new
-    expect(card.balance).to eq 0
+    expect(subject.balance).to eq 0
+  end
+
+  it 'allows you to top it up' do
+    expect(subject).to respond_to(:top_up).with(1).argument  
+  end
+
+  it 'should top up a card' do
+    expect { subject.top_up 20 }.to change { subject.balance }.by 20
   end
 end
